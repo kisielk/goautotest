da3f99d58d76cd00419fd0730fa05e51d91bbf1d
Kamil Kisiel <kamil@kamilkisiel.net>
2013-07-15 23:15:35 -0700

    Add logging.
    For each .go file that receives an event during the debounce period,
    log the the last received event.

--- goautotest.go
+++ goautotest.go
@@ -3,68 +3,90 @@ package main
 import (
 	"fmt"
 	"github.com/howeyc/fsnotify"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
-func startGoTest() {
-	fmt.Println("Running tests...")
-
-	args := append([]string{"test"}, os.Args[1:]...)
-	cmd := exec.Command("go", args...)
+func startGoTest(args ...string) error {
+	cmd := exec.Command("go", append([]string{"test"}, args...)...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	err := cmd.Start()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("error starting go test: %s", err)
 	}
 	err = cmd.Wait()
 	if err != nil {
-		fmt.Println(err)
+		return fmt.Errorf("error running go test: %s", err)
 	}
-
-	fmt.Println()
+	return nil
 }
 
 func main() {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
 	wd, err := os.Getwd()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal("failed to get working directory:", err)
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal("failed to initialize fsnotify:", err)
 	}
 
 	err = watcher.Watch(wd)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal("failed to watch working directory:", err)
 	}
-
 	defer watcher.Close()
 
-	burstGuard := make(<-chan time.Time)
+	events := make(map[string]*fsnotify.FileEvent)
+	debounce := make(<-chan time.Time)
 	for {
 		select {
 		case ev := <-watcher.Event:
-			if strings.HasSuffix(ev.Name, ".go") {
-				burstGuard = time.After(500 * time.Millisecond)
+			name, err := filepath.Rel(wd, ev.Name)
+			if err != nil {
+				name = ev.Name
 			}
-
+			events[name] = ev
+			debounce = time.After(500 * time.Millisecond)
 		case err := <-watcher.Error:
-			fmt.Println(err)
-
-		case <-burstGuard:
-			startGoTest()
+			log.Println("watcher error:", err)
+		case <-debounce:
+			var runTests bool
+			for k, v := range events {
+				if strings.HasSuffix(k, ".go") {
+					log.Println(eventDesc(v), k)
+					runTests = true
+				}
+			}
+			if runTests {
+				err := startGoTest(os.Args[1:]...)
+				if err != nil {
+					log.Println(err)
+				}
+			}
+			events = make(map[string]*fsnotify.FileEvent)
 		}
 	}
+}
 
+func eventDesc(ev *fsnotify.FileEvent) string {
+	switch {
+	case ev.IsCreate():
+		return "created"
+	case ev.IsDelete():
+		return "removed"
+	case ev.IsModify():
+		return "modified"
+	case ev.IsRename():
+		return "renamed"
+	default:
+		return ""
+	}
 }
